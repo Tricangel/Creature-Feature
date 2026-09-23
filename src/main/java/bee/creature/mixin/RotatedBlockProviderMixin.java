ca68e5b4ea6a64f70a2f423149cf02d09849abf4
Bee <146317619+Tricangel@users.noreply.github.com>
2026-09-23 17:27:22 +1200

    Port to 26.3

--- src/main/java/bee/creature/mixin/RotatedBlockProviderMixin.java
+++ src/main/java/bee/creature/mixin/RotatedBlockProviderMixin.java
@@ -4,6 +4,7 @@ import bee.creature.registry.BlockStateProviderOverrides;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.stateproviders.RandomizedIntStateProvider;
@@ -14,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(RotatedBlockProvider.class)
 public class RotatedBlockProviderMixin {
 	@ModifyReturnValue(at = @At("RETURN"), method = "getState")
-	private BlockState init(BlockState original, WorldGenLevel level, RandomSource randomSource, BlockPos blockPos) {
-		BlockState state = BlockStateProviderOverrides.shouldOverride(original, randomSource);
+	private BlockState init(BlockState original, final LevelAccessor level, final RandomSource random, final BlockPos pos) {
+		BlockState state = BlockStateProviderOverrides.shouldOverride(original, random);
 
 		if (state != null) {
 			return state;
